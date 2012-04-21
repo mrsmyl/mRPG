95ac8f976880ce01b19370c7bf5a6337b0adb270
Richard <richard@mozor.net>
2012-04-21 17:24:54 -0500

    Added beginnings of sqlite

--- mrpg.py
+++ mrpg.py
@@ -12,10 +12,18 @@
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol, task
 from twisted.python import log
+from twisted.enterprise.adbapi import ConnectionPool
+from twisted.internet.defer import DeferredList
 
 # system imports
 import time, sys
 import ConfigParser
+
+timespan = 10.0
+
+def printResult(result):
+	for r in result:
+		print(r[1])
 
 class mrpg:
 	def __init__(self, parent):
@@ -28,7 +36,7 @@ class mrpg:
 		self.msg("Please standby")
 
 		# Start the reactor task that repeatedly loops through actions
-		self.l.start(10.0) # call every 10 seconds for now
+		self.l.start(timespan) # call every 10 seconds for now
 
 		self.msg("Initialization complete")
 
@@ -45,6 +53,53 @@ class mrpg:
 
 	def rpg(self):
 		self.msg("This is the looping call that will do stuff")
+		self.db = DBPool('mrpg.db')
+		self.db.update_user_time('richard',timespan * -1)
+		self.db.shutdown("")
+
+class User:
+	def __init__(self, username, char_class, password):
+		self.username = username
+		self.char_class = char_class
+		self.password = password
+	def render(self):
+		msg = "Mr '%s %s' is connected under '%s'" % (self.password, 
+														 self.char_class,
+														 self.username,)
+		return msg
+class DBPool:
+	"""
+		Sqlite connection pool
+	"""
+	def __init__(self, dbname):
+		self.dbname = dbname
+		self.__dbpool = ConnectionPool('sqlite3', self.dbname, check_same_thread=False)
+
+	def shutdown(self, callback_msg):
+		"""
+			Shutdown function
+			It's a required task to shutdown the database connection pool:
+				garbage collector doesn't shutdown associated thread
+		"""
+		self.__dbpool.close()
+		
+	def build_user(self, dbentries):
+		"""
+			Build user from dbentries
+		"""
+		username, char_class, password = dbentries[0]
+		return User(username, char_class, password)
+	def get_user_user(self, username):
+		"""
+			Build associated user object
+		"""
+		query = 'SELECT username, char_class, password from `users` where username=?'
+		return self.__dbpool.runQuery(query, (username,)).addCallback(
+												  self.build_user)
+	def update_user_time(self, username, time):
+		time = str(time)
+		query = 'UPDATE `users` SET tonextlevel = tonextlevel + ' + time + ' where username=?'
+		return self.__dbpool.runQuery(query, (username,))
 
 class Bot(irc.IRCClient):
 	"""A logging IRC bot."""
@@ -98,7 +153,6 @@ class Bot(irc.IRCClient):
 	def action(self, user, channel, msg):
 		"""This will get called when the bot sees someone do an action."""
 		user = user.split('!', 1)[0]
-		self.mrpg.performPenalty(user, "channel action")
 
 	# irc callbacks
 
@@ -106,7 +160,6 @@ class Bot(irc.IRCClient):
 		"""Called when an IRC user changes their nickname."""
 		old_nick = prefix.split('!')[0]
 		new_nick = params[0]
-		self.mrpg.performPenalty(user, "nickname change")
 
 
 	# For fun, override the method that determines how a nickname is changed on
@@ -135,7 +188,21 @@ class BotFactory(protocol.ClientFactory):
 		self.nickname = nickname
 		self.server = server
 		self.port = port
-		self.db = 'mrpg.db'
+		# self.db = DBPool('mrpg.db')
+		
+		# DeferredList seems more adapted than chained callbacks in this sort of cases
+		# ret_render = lambda user: user.render()
+		# deferreds = [self.db.get_user_user(username).addCallback(ret_render)
+		# 						for username in ('richard','newtoz')]
+
+		# dlist = DeferredList(deferreds)
+		# dlist.addCallback(printResult)
+		# We ask our pool to shutdown all the initialized connections
+		# dlist.addCallback(self.db.shutdown)
+
+
+
+
 
 	def buildProtocol(self, addr):
 		p = Bot()
