e286e607a9c096673617845c9fa93f42a70e3693
Richard <richard@mozor.net>
2012-04-24 20:05:03 -0500

    Basic TTL functionality complete, closes issue #1

--- mrpg.py
+++ mrpg.py
@@ -13,14 +13,14 @@ from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol, task
 from twisted.python import log
 from twisted.enterprise import adbapi
-# from twisted.internet.defer import DeferredList
 
 # system imports
 import time, sys
 import ConfigParser
 
 timespan = 10.0
-min_time = 30
+min_time = 5
+penalty_constant = 1.1
 is_started = 0
 
 def printResult(result):
@@ -58,8 +58,8 @@ class mrpg:
 	def performPenalty(self, user, reason):
 		if is_started == 1:
 			#commented out due to the spam it creates in IRC and just make it print to console
-			#self.msg(user + " has earned a penalty for: " + reason)
-			print "Penalty"
+			self.msg(user + " has earned a penalty for: " + reason)
+			self.db.updateUserTimeMultiplier(user, penalty_constant)
 
 	def rpg(self):
 		#commented out due to the spam it creates in IRC and just make it print to console
@@ -101,17 +101,17 @@ class DBPool:
 		self.__dbpool.close()
 
 	def getUser(self):
-		query = "SELECT * FROM users"
+		query = "SELECT * FROM users WHERE online = 1"
 		s = self.__dbpool.runQuery(query).addCallback(self.showUser)
 	
 	def showUser(self, output):
 		for i in output:
 			message = str(i[1]) + " will reach the next level in " + str(i[4]) + " seconds."
-			# self.mrpg.msg(message)
+			self.mrpg.msg(message)
 			print message
 
 	def levelUp(self):
-		query = "SELECT username, level, ttl FROM users WHERE ttl < 0"
+		query = "SELECT username, level, ttl FROM users WHERE ttl < 0 AND online = 1"
 		s = self.__dbpool.runQuery(query).addCallback(self.showLevelUp)
 	
 	def showLevelUp(self, output):
@@ -142,10 +142,13 @@ class DBPool:
 		return self.__dbpool.runQuery("UPDATE users SET ttl = ?, level = ? WHERE username = ?", (ttl, new_level, user, ))
 
 	def updateUserTime(self, user, amount):
-		return self.__dbpool.runQuery("UPDATE users SET ttl = ttl + ? WHERE username = ?", (amount, user,))
+		return self.__dbpool.runOperation("UPDATE users SET ttl = ttl + ? WHERE username = ?", (amount, user,))
+
+	def updateUserTimeMultiplier(self, user, amount):
+		return self.__dbpool.runOperation("UPDATE users SET ttl = ROUND(ttl * ?,0) WHERE username = ?", (amount, user,))
 
 	def updateAllUsersTime(self, amount):
-		return self.__dbpool.runQuery("UPDATE users SET ttl = ttl + ?", (amount,))
+		return self.__dbpool.runOperation("UPDATE users SET ttl = ttl + ? WHERE online = 1", (amount,))
 
 	def register_user(self, reg_username, reg_password, reg_char_class):
 		query = 'INSERT INTO `users` (id,username,password,level,ttl,char_class,hostname,online) VALUES (NULL,?,?,1,?,?,NULL,1)'
@@ -183,25 +186,27 @@ class Bot(irc.IRCClient):
 		"""This will get called when the bot receives a message."""
 		user = user.split('!', 1)[0]
 		
-                # Check to see if they're sending me a private message
-                if channel == self.nickname:
-                        # msg_out = "It isn't nice to whisper!  Play nice with the group."
-                        #self.notice(user, msg_out)
-                        #self.msg(self.factory.channel, msg)
-                        if msg.lower() == "register":
-                                self.notice(user, "To register: /msg mBot register <char name> <password>  <char class>")
-                        else:
-                                msg_split = msg.split(' ')
-                                #ASOLUTELY NO ERROR CORRECTION YET!!
-                                if msg_split[0] == "register":
-                                        reg_username = msg_split[1]
-                                        reg_password = msg_split[2]
-                                        reg_char_class = msg_split[3::1]
-                                        reg_char_class = ' '.join(reg_char_class)
-                                        self.db = DBPool('mrpg.db')
-                                        self.db.register_user(reg_username,reg_password,reg_char_class)
-                                        self.db.shutdown("")
-                                        self.notice(user, "Created new character " + reg_username)
+		# Check to see if they're sending me a private message
+		if channel == self.nickname:
+			# msg_out = "It isn't nice to whisper!  Play nice with the group."
+			#self.notice(user, msg_out)
+			#self.msg(self.factory.channel, msg)
+			if msg.lower() == "register":
+				self.notice(user, "To register: /msg mBot register <char name> <password>  <char class>")
+			else:
+				msg_split = msg.split(' ')
+				#ASOLUTELY NO ERROR CORRECTION YET!!
+				if msg_split[0] == "register":
+					reg_username = msg_split[1]
+					reg_password = msg_split[2]
+					reg_char_class = msg_split[3::1]
+					reg_char_class = ' '.join(reg_char_class)
+					self.db = DBPool('mrpg.db')
+					self.db.register_user(reg_username,reg_password,reg_char_class)
+					self.db.shutdown("")
+					self.notice(user, "Created new character " + reg_username)
+		else:
+			self.mrpg.performPenalty(user, "channel message")
 
 		# Otherwise check to see if it is a message directed at me
 		if msg.startswith(self.nickname + ":"):
